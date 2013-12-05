d3e3ef3817c793a4865cb66d541852c2aa7bb314
Alexey <alexey.shishkovets@gmail.com>
2013-12-05 17:27:35 +0300

    CreditCalculator is adapted for model

--- service/src/main/java/org/kofi/creditex/Dates.java
+++ service/src/main/java/org/kofi/creditex/Dates.java
@@ -1,10 +1,13 @@
 package org.kofi.creditex;
 
-public class DateProvider {
+public class Dates {
     public static java.util.Date nowUtil(){
         return new java.util.Date();
     }
     public static java.sql.Date now(){
         return new java.sql.Date(nowUtil().getTime());
+    }
+    public static java.sql.Date now(int days_shift){
+        return new java.sql.Date(nowUtil().getTime() + (long)days_shift*(24*3600*1000));
     }
 }
---
+++ service/src/test/java/UserTest.java
@@ -0,0 +1,33 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.kofi.creditex.Dates;
+import org.kofi.creditex.service.CreditCalculator;
+import org.kofi.creditex.service.UserService;
+import org.kofi.creditex.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import org.kofi.creditex.model.*;
+
+import java.util.List;
+
+@ContextConfiguration(locations = "classpath:service-test-context.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional(propagation = Propagation.REQUIRED)
+public class UserTest {
+    @Autowired
+    private UserDetailsService userService;
+
+    @Test
+    public void SimpleTest(){
+        userService.loadUserByUsername("SomeName");
+
+    }
+}
+
