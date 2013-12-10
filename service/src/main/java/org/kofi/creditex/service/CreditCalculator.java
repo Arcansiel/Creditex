22e82953053512e05fef0ff7ebd7f156cebf25c8
Alexey <alexey.shishkovets@gmail.com>
2013-12-10 18:05:24 +0300

    bad code removed

--- service/src/main/java/org/kofi/creditex/service/CreditCalculator.java
+++ service/src/main/java/org/kofi/creditex/service/CreditCalculator.java
@@ -64,61 +64,6 @@ public class CreditCalculator {
         return PaymentPlan(credit.getProduct(), credit.getOriginalMainDebt(), credit.getDuration(), credit.getStart(), out);
     }
 
-
-    /*
-    private static FineType ToFineType(PriorRepayment priorRepayment){
-        FineType fineType;
-        if(priorRepayment.equals(PriorRepayment.AvailableFineInterest)){
-            fineType = FineType.Interest;
-        }else if(priorRepayment.equals(PriorRepayment.AvailableFinePercentSum)){
-            fineType = FineType.Percents;
-        }else{
-            fineType = FineType.None;
-        }
-        return fineType;
-    }
-
-    public static boolean IsAvailablePriorRepayment(Credit credit){
-        if(credit.getProduct().getPrior().equals(PriorRepayment.NotAvailable)){
-            return false;
-        }else{
-            float debt_limit = credit.getOriginalMainDebt() * credit.getProduct().getPriorRepaymentDebtLimit();
-            return credit.getCurrentMainDebt() <= debt_limit;
-        }
-    }
-
-    //return[0] - полная сумма платежа со штрафом, return[1] - сумма процентов, return[2] - сумма штрафа
-    public static int[] PriorRepaymentSum(Credit credit){
-        FineType fine_type = ToFineType(credit.getProduct().getPrior());
-        double fine_value = credit.getProduct().getPriorRepaymentPercent();
-        long[] r = CreditCalcBase.PriorRepayment(credit.getCurrentMainDebt(),credit.getProduct().getPercent(),fine_type,fine_value);
-        int[] out = new int[3]; out[0] = (int)r[0]; out[1] = (int)r[1]; out[2] = (int)r[2];
-        return out;
-    }
-
-    //платёж с учётом пени за задержку
-    //return[0] - сумма платежа со штрафом, return[1] - сумма штрафа
-    public static int[] PaymentSum(Payment payment, java.sql.Date now, float fine){
-        java.util.Date deadline = new java.util.Date(payment.getPaymentEnd().getTime());
-        long[] r = CreditCalcBase.DelayFine(payment.getRequiredPayment(), deadline, new java.util.Date(now.getTime()), fine);
-        int[] out = new int[2]; out[0] = (int)r[0]; out[1] = (int)r[1];
-        return out;
-    }
-
-    //платёж с учётом пени за задержку каждого платежа
-    //return[0] - сумма платежей со штрафом, return[1] - общая сумма штрафа
-    public static int[] TotalPaymentSum(Iterable<Payment> payments, java.sql.Date now, float fine){
-        int[] out = new int[2]; out[0] = 0; out[1] = 0;
-        for(Payment p:payments){
-            if(!p.isPaymentClosed()){
-                int[] r = PaymentSum(p, now, fine);
-                out[0] += r[0]; out[1] += r[1];
-            }
-        }
-        return out;
-    }
-    */
-
     private static boolean ValidRequiredProduct(Product product,
                                           int sum_required, int duration, int max_payment){
         if(sum_required < product.getMinMoney()
--- service/src/main/java/org/kofi/creditex/service/base/FineType.java
+++
@@ -1,8 +0,0 @@
-package org.kofi.creditex.service.base;
-
-//тип штрафа при досрочном погашении
-public enum FineType {
-    Interest,//штраф на процентную ставку
-    Percents,//штраф на сумму процентов
-    None//без штрафа
-}
