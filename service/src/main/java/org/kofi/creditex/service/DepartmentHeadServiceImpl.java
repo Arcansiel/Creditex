38cdc150d39d4fc2167012db910544709fba3e60
Alexey <alexey.shishkovets@gmail.com>
2013-12-16 11:02:35 +0300

    prolongation execution deleted from head service

--- service/src/main/java/org/kofi/creditex/service/DepartmentHeadServiceImpl.java
+++ service/src/main/java/org/kofi/creditex/service/DepartmentHeadServiceImpl.java
@@ -137,14 +137,6 @@ public class DepartmentHeadServiceImpl implements DepartmentHeadService {
             return -2;//not in progress
         }
         p.setAcceptance(acceptance_value);
-
-        //TODO (test it) Credit prolongation call
-        if(acceptance){
-            if(creditService.ExecuteProlongation(p.getCredit().getId(),p.getDuration()) != 0){
-                return -3;//prolongation failed
-            }
-        }
-
         prolongationRepository.save(p);
 
         return 0;
