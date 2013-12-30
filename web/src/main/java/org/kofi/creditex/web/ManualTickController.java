06a24e5591c0d44a3966deff2e1a40789c5a2460
Alexey <alexey.shishkovets@gmail.com>
2013-12-30 19:26:05 +0300

    manual ticks : 1..365 days

--- web/src/main/java/org/kofi/creditex/web/ManualTickController.java
+++ web/src/main/java/org/kofi/creditex/web/ManualTickController.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @Slf4j
@@ -21,8 +24,24 @@ public class ManualTickController {
     }
 
     @RequestMapping(value = {"/tick/do/","/tick/do"}, method = {RequestMethod.GET, RequestMethod.POST})
-    public String DoTick(){
-        dateProvider.IncreaseDate();
-        return "redirect:/tick/";
+    public String DoTick(
+            HttpSession session
+            ,@RequestParam(value = "count", required = false, defaultValue = "1") int count
+    ){
+        if(count < 1){
+            return "redirect:/tick/?info=0";
+        }
+        if(count > 365){
+            count = 365;
+        }
+        if(session.getAttribute("manual_tick_is_running") != null){
+            return "redirect:/tick/?info=previous_tick_is_running";
+        }
+        session.setAttribute("manual_tick_is_running",count);
+        for(int i = 0; i < count; i++){
+            dateProvider.IncreaseDate();
+        }
+        session.removeAttribute("manual_tick_is_running");
+        return "redirect:/tick/?info="+count;
     }
 }
